fc0789fc9a453e9d2a6696d7c93f0e25729c873f
SvitlanaRet <s.retunska@gmail.com>
2023-10-22 19:21:18 +0300

    fix

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Get the button:
+
 let buttonTop = document.getElementById("buttonTop");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop >500 || document.documentElement.scrollTop > 500) {
     buttonTop.style.display = "block";
   } else {
     buttonTop.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document with smooth animation
+
 function topFunction() {
     smoothScrollToTop();
 }
